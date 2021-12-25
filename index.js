3e84a4e5a4886eecef21865857009c3f7fdbbc38
Alicia <arrowsrp@yahoo.com>
2021-12-25 00:41:28 -0800

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+const { arrayContaining } = require("expect")
+
+function findMatching(array, str){
+    let driverList = array.filter(name => name.toLowerCase() === str.toLowerCase())
+    return driverList
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(array => array.startsWith(string))
+}
+
+function matchName(array, str){
+    return array.filter(array => array.name === str)}
